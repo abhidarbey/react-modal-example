eb2fcff9c69c02a9ef379ad09411a91769d8dda5
Abhinandan Darbey <abhinandan.darbey@gmail.com>
2020-04-27 22:57:51 +0530

    Clear clutter

--- src/views/Resolution.js
+++ src/views/Resolution.js
@@ -7,13 +7,10 @@ import { SamplePage } from './SamplePage';
 export const Resolution = () => {
 
     const [modalIsOpen, setModalIsOpen] = useState(false)
-    // const [modalId, setModalId] = useState(null)
     const [modalData, setModalData] = useState({})
-    let samplePage;
 
     const clickOpen = (element) => {
         setModalIsOpen(true)
-        // setModalId(element.id)
         setModalData(element)
     }
 
@@ -33,31 +30,26 @@ export const Resolution = () => {
         <td>{element.value}</td>
         <td>{element.status}</td>
         <td>
-            <button className="btn btn-primary"
-                    onClick={() => clickOpen(element)}>
-                        Add Resolution
-            </button>
-            
-        </td>
-        {/* <td>
           <div className="btn-group" role="group">
                 <button id="btnGroupDrop1" type="button" className="btn btn-secondary dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                     Action
                 </button>
                 <div className="dropdown-menu" aria-labelledby="btnGroupDrop1">
-                    <button className="btn btn-primary" onClick={clickOpen}>Add Resolution</button>
-                    <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                        <SamplePage data={element} name="Pune" />
-                    </Modal>
-                    <button className="btn btn-primary">Resolution Details</button>
+                    <button className="btn btn-primary" onClick={() => clickOpen(element)}>
+                        Add Resolution
+                    </button>
+                    <button className="btn btn-primary">
+                        Resolution Details
+                    </button>
                 </div>
          </div>
-        </td> */}
+        </td>
       </tr>
     ));
 
     return (
         <div className="container">
+            
             <table className="table table-hover">
                 <thead>
                     <tr>
@@ -77,17 +69,14 @@ export const Resolution = () => {
                     {revenueMVPD}
                 </tbody>
             </table>
+            
             <Modal isOpen={modalIsOpen} onRequestClose={() => setModalIsOpen(false)}>
-                <SamplePage
-                    show={modalIsOpen}
-                    data={modalData}
-                    revenueListNetwork={revenueListNetwork}
-                  />
-                  <button className="btn btn-primary"
-                          onClick={clickClose}>
-                              Close
+                <SamplePage data={modalData} revenueListNetwork={revenueListNetwork} />
+                  <button className="btn btn-primary" onClick={clickClose}>
+                        Close
                   </button>
             </Modal>
+            
         </div>
     )
 }
